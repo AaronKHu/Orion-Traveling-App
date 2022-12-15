3306191834ef32ecea9def97c81ec28dd1733d22
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-12-14 20:17:09 -1000

    Added navigators for the tab section wiht respective components

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import Home from './components/Home';
+import Details from './components/Details';
+import Liked from './components/Liked';
+import Profile from './components/Profile';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+//Everything on Home page
+const TabNavigator = () => {
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name="home" component={Home} />
+      <Tab.Screen name="Liked" component={Liked} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+const App = () => {
+  return (
+    <View>
+        <Text>App.js</Text>
+    </View>
+  )
+}
+
+export default App;
