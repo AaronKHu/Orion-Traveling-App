6f714e7738668677f81240ac070e0205ca12cfe7
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-12-16 19:58:33 -1000

    Installed react native npms and adjusted component

--- App.js
+++ App.js
@@ -7,9 +7,9 @@ import Details from './components/Details';
 import Liked from './components/Liked';
 import Profile from './components/Profile';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,14 +18,12 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return(
     <Tab.Navigator>
-      <Tab.Screen name="home" component={Home} />
+      <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Liked" component={Liked} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   )
 }
-
-
 
 const App = () => {
   return (
